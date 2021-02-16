73535ed1269cb7b615d0eb4c03d428e9698083cb
Rafael Silva <rasilva.1986@gmail.com>
2021-02-16 04:46:15 -0800

    Declared the variables and printed them

--- FirstExample.java
+++ FirstExample.java
@@ -1,5 +1,11 @@
 public class FirstExample{
     public static void main(String[] args){
+        int a = 10;
+        int b = 20;
 
+        System.out.println("A:" +a);
+        System.out.println("B:" +b);
+
+        //Swap the values
     }
 }
